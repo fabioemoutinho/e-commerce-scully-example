9322b4f445690e34f4343a1f17b9336744fcd1c9
fabioemoutinho <fabioemoutinho@gmail.com>
2022-03-01 18:55:37 -0300

    add product plugin for partial scully build step

--- scully.e-commerce-scully-example.config.ts
+++ scully.e-commerce-scully-example.config.ts
@@ -2,8 +2,7 @@ import { ScullyConfig } from '@scullyio/scully';
 
 /** this loads the default render plugin, remove when switching to something else. */
 import '@scullyio/scully-plugin-puppeteer';
-import { Product } from './src/app/product/product.model';
-import { environment } from './src/environments/environment.prod';
+import './scully/plugins/plugin';
 
 export const config: ScullyConfig = {
   projectRoot: './src',
@@ -15,15 +14,7 @@ export const config: ScullyConfig = {
   outDir: './dist/static',
   routes: {
     '/product/:slug': {
-      type: 'json',
-      slug: {
-        url: `${environment.api.url}/products`,
-        property: 'id',
-        resultsHandler: (data: Product[]) => {
-          console.log(data);
-          return Array.isArray(data) ? data : [];
-        },
-      },
+      type: 'product',
     },
   },
 };
--- scully/plugins/plugin.ts
+++ scully/plugins/plugin.ts
@@ -1,12 +1,31 @@
+import { HandledRoute, registerPlugin, httpGetJson } from '@scullyio/scully';
+import { Product } from '../../src/app/product/product.model';
+import { environment } from '../../src/environments/environment.prod';
 
-import { registerPlugin, getPluginConfig } from '@scullyio/scully';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 
-export const myPlugin = 'myPlugin';
+export const product = 'product';
 
-const myFunctionPlugin = async (html: string): Promise<string> => {
-  return html;
+const productRoutes = async (route?: string): Promise<HandledRoute[]> => {
+  const yarg = yargs(hideBin(process.argv));
+  const argv = await yarg.option('productIds', { type: 'string' }).argv;
+
+  // if there are --productIds being passed in npx scully --productIds=1,2,3
+  // then only generate static files for those products
+  // else fetch all products from API and map the returned IDs.
+  const productIds: string[] = argv.productIds
+    ? argv.productIds.split(',')
+    : ((await httpGetJson(`${environment.api.url}/products`)) as Product[]).map(
+        (product) => product.id.toString()
+      );
+  const productRoutes: HandledRoute[] = productIds.map((id) => ({
+    route: `/product/${id}`,
+  }));
+
+  return Promise.resolve(productRoutes);
 };
 
 const validator = async () => [];
 
-registerPlugin('postProcessByHtml', myPlugin, myFunctionPlugin, validator);
+registerPlugin('router', product, productRoutes, validator);
